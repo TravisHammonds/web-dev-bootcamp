8c77eae6d708a6b30bcb55734447652ba9c21faa
Travis Hammonds <travis@Traviss-MacBook-Air.local>
2022-05-10 14:54:38 -0400

    eventListener added to buttons

---
+++ DrumKit-AdvJs/Drum Kit Starting Files/index.js
@@ -0,0 +1,20 @@
+
+// var numberOfButtons = document.querySelectorAll(".drum").length;
+
+// for (var i = 0; i <numberOfButtons; i++) {
+
+//     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+//         alert("I got clicked!");
+//     });
+
+// }
+
+const matches = document.querySelectorAll(".set > button ");
+
+matches.addEventListener("click", function () {
+    alert("I got clicked!");
+});
+
+
+//     alert("I got clicked!");
+// });
